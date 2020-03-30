f22387716fa272810b8214c78de2edc69fd25945
Shahan Neda <shahan.neda@gmail.com>
2020-03-29 23:22:57 -0700

    reorganised project

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        Physics.Raycast(ray, out hit);
+        if (hit.collider != null) { 
+            print(hit.collider.gameObject.transform.name);
+        }
+    }
+}
